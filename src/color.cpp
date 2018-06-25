a3a9fb99e02e361a1ec2d3fb739495bfa358986f
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-06-25 12:10:03 +0300

    Add rgb color

--- src/color.cpp
+++ src/color.cpp
@@ -11,36 +11,36 @@ vector<int> getColor(
         vector<double> &normal,
         vector<int> &rgb
 ) {
-//    vector<double> direction(3, 0);
-//    for (int i = 0 ; i < 3 ; i++) {
-//        direction[i] = point[i] - camera[i];
-//    }
-//
-//    vector<double> normDir = normalize(direction);
-//    vector<double> d = multiply(normDir, distance);
-//
-//    vector<double> target(3, 0);
-//    for (int i = 0 ; i < 3 ; i++) {
-//        target[i] = camera[i] + d[i];
-//    }
-//
-//    vector<double> lightV(3, 0);
-//    for (int i = 0 ; i < 3 ; i++) {
-//        lightV[i] = light[i] - target[i];
-//    }
-//
-//    double angleCos = cosBetweenVectors(normal, lightV);
-//
-    vector<double> n = normalize(normal);
+    vector<double> direction(3, 0);
+    for (int i = 0 ; i < 3 ; i++) {
+        direction[i] = point[i] - camera[i];
+    }
+
+    vector<double> normDir = normalize(direction);
+    vector<double> d = multiply(normDir, distance);
+
+    vector<double> target(3, 0);
+    for (int i = 0 ; i < 3 ; i++) {
+        target[i] = camera[i] + d[i];
+    }
+
+    vector<double> lightV(3, 0);
+    for (int i = 0 ; i < 3 ; i++) {
+        lightV[i] = light[i] - target[i];
+    }
+
+    double angleCos = cosBetweenVectors(normal, lightV);
+
 
     vector<int> color(3, 0);
-//    for (int i = 0 ; i < 3 ; i++) {
-//        color[i] = int(rgb[i] - rgb[i]*0.7*angleCos);
-//    }
-
     for (int i = 0 ; i < 3 ; i++) {
-        color[i] = int(235 - 180*abs(n[i]));
+        color[i] = int(rgb[i] - rgb[i]*0.7*angleCos);
     }
+
+//    vector<double> n = normalize(normal);
+//    for (int i = 0 ; i < 3 ; i++) {
+//        color[i] = int(235 - 180*abs(n[i]));
+//    }
 
     return color;
 }
